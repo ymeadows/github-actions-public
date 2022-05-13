17bdb7a1374894e2d565587bd4b8a7aeaea54c9b
Judson Lester <nyarly@gmail.com>
2022-05-12 22:07:20 -0700

    Extend wait to 10 minutes DOP-694

--- launch-self-hosted-runner/action.sh
+++ launch-self-hosted-runner/action.sh
@@ -90,7 +90,7 @@ EOS
   echo "::set-output name=label::${VM_ID}"
 
   safety_off
-  while (( i++ < 24 )); do
+  while (( i++ < 120 )); do
     GH_READY=$(gcloud compute instances describe ${VM_ID} --zone=${machine_zone} --format='json(labels)' | jq -r .labels.gh_ready)
     if [[ $GH_READY == 1 ]]; then
       break
