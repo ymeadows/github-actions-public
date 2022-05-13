5c78146b79067d6fcce2818848e5b060a7398150
Judson Lester <nyarly@gmail.com>
2022-05-13 15:43:10 -0700

    Configure runner as runner user DOP-694

--- launch-self-hosted-runner/action.sh
+++ launch-self-hosted-runner/action.sh
@@ -66,9 +66,10 @@ EOS
 
   cat <<EOS >>$startup_script
 chown runner -RL /actions-runner
+ls -l /
 ls -l /actions-runner
 gcloud compute instances add-labels ${VM_ID} --zone=${machine_zone} --labels=gh_ready=0 && \\
-RUNNER_ALLOW_RUNASROOT=1 ./config.sh --url https://github.com/${GITHUB_REPOSITORY} --token ${RUNNER_TOKEN} --labels ${labels} --unattended --ephemeral --disableupdate && \\
+su runner -c "./config.sh --url https://github.com/${GITHUB_REPOSITORY} --token ${RUNNER_TOKEN} --labels ${labels} --unattended --ephemeral --disableupdate" && \\
 ls /home
 cat /etc/passwd
 ./svc.sh install runner && \\
