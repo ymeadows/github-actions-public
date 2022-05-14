fb5fc236edd0827146276f1ac72becce735ff5c2
Judson Lester <nyarly@gmail.com>
2022-05-13 17:36:19 -0700

    Get metadata on the VM DOP-694

--- launch-self-hosted-runner/action.sh
+++ launch-self-hosted-runner/action.sh
@@ -66,9 +66,9 @@ EOS
   fi
 
   cat <<EOS >>$startup_script
-NAME=$(curl -S -s -X GET http://metadata.google.internal/computeMetadata/v1/instance/name -H 'Metadata-Flavor: Google')
-ZONE=$(curl -S -s -X GET http://metadata.google.internal/computeMetadata/v1/instance/zone -H 'Metadata-Flavor: Google')
-echo "sleep 30; $(which gcloud) --quiet compute instances delete $NAME --zone=$ZONE" >> /shutdown-vm.sh
+NAME=\$(curl -S -s -X GET http://metadata.google.internal/computeMetadata/v1/instance/name -H 'Metadata-Flavor: Google')
+ZONE=\$(curl -S -s -X GET http://metadata.google.internal/computeMetadata/v1/instance/zone -H 'Metadata-Flavor: Google')
+echo "sleep 30; \$(which gcloud) --quiet compute instances delete $NAME --zone=$ZONE" >> /shutdown-vm.sh
 echo "ACTIONS_RUNNER_HOOK_JOB_COMPLETED=/shutdown-vm.sh" >> /actions-runner/.env
 chown runner -RL /actions-runner
 gcloud compute instances add-labels ${VM_ID} --zone=${machine_zone} --labels=gh_ready=0 && \\
