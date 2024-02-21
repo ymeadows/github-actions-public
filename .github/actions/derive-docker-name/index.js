e0957c110fda17e1bd00be0e7d5c1df9a02b252f
Adam Rich <adam.rich@ymeadows.com>
2024-02-21 14:23:21 -0800

    YM-5813
    - Update step code to handle if "ymeadows/" was not included
    - Update dependencies

--- .github/actions/derive-docker-name/index.js
+++ .github/actions/derive-docker-name/index.js
@@ -1,13 +1,21 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const repositoryNameSeparator = "/";
+const stepRepoNamePrefix = "step-";
+const emptyString = "";
+
+
 try {
     let repoName = core.getInput('repo-name');
     if(!repoName) {
         repoName = github.context.payload.repository.name;
     }
-    repoName = repoName.split("/")[1];
-    const dockerImageName = repoName.replace("step-", "");
+    if(repoName.includes(repositoryNameSeparator)) {
+        repoName = repoName.split(repositoryNameSeparator)[1];
+    }
+    core.info(`Repo Name is ${repoName}`);
+    const dockerImageName = repoName.replace(stepRepoNamePrefix, emptyString);
     core.info(`Docker Image Name is ${dockerImageName}`);
     core.setOutput("docker-name", dockerImageName);
 } catch (error) {
